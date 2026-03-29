b6071cb9eccd674668735b9065d68122caa47762
MREZA <rezaworku31@gmail.com>
2026-03-29 15:09:38 +0700

    chore/product-pages

--- berkahpagi_fe/src/routes/index.tsx
+++ berkahpagi_fe/src/routes/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Layout from '../components/Layout'
 import Home from '../pages/Home'
-import Products from '../pages/Products'
 import Vendor from '../pages/Vendor'
 import Staff from '../pages/Staff'
 import Login from '../pages/Login'
 import ProductDetails from '../pages/ProductDetails'
+import ProductPage from '../pages/ProductPage'
 
 export default function AppRoutes() {
     return (
@@ -14,7 +14,7 @@ export default function AppRoutes() {
             <Routes>
                 <Route element={<Layout />}>
                     <Route element={<Home />} path='/' />
-                    <Route element={<Products />} path='/products' />
+                    <Route element={<ProductPage />} path='/products' />
                     <Route element={<Vendor />} path='/vendor' />
                     <Route element={<Staff />} path='/staff' />
                     <Route element={<ProductDetails />} path='/product/:id' />
